e357a9b02c3b1de68ec52e45becf943412f5c11c
VivianTCodes <70532616+VivianTCodes@users.noreply.github.com>
2020-10-12 23:46:21 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <Weather />
+        </header>
 
-      <footer>
-        This app is coded by Vivian Tran and is{" "}
-        <a href="https://github.com/VivianTCodes/weather_react" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+        <footer>
+          This app is coded by Vivian Tran and is{" "}
+          <a
+            href="https://github.com/VivianTCodes/weather_react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
